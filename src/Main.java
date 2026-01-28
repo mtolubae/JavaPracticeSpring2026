368fb6688f708c556f0d770605838c38135c93ce
mtolubae <noviy.mail@gmail.com>
2026-01-28 09:46:46 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        int x = 0;
+//        double d = 3.14;
+//        float f = 3.14F;
+//        long l = 123456789012345678L;
+//        String s1 = "Hello";
+//        String s2 = "Hello";
+//        System.out.println(s1 == s2);
+//        System.out.println(s1.equals(s2));
+//        String s3 = s2;
+//        System.out.println(s3.equals(s1));
+
+        int arg1 = Integer.parseInt(args[0]);
+        int arg2 = Integer.parseInt(args[1]);
+        System.out.println(arg1+arg2);
+    }
+}
